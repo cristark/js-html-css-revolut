e58abd5039c120fec903dd2627ba85d942d5f675
Cristiano <cristiano.tarconi@gmail.com>
2021-01-14 00:22:51 +0100

    language menu show click jquery

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 
     
-        $('.cta_nav .dropdown').click(function(){
-            $(this).children('.drop_down:language').toggleClass('active');
-        });
+    $('.cta_dropdown').click(function(){
+        $(this).children('.drop_down.language').toggleClass('active');
+    });
+    
+    $('.cta_nav').mouseleave(function(){
+        $(this).find('.drop_down.language').removeClass('active');
+    });
 
     $('.dropdown').hover(function(){
         $(this).children('.drop_down').toggleClass('active');
